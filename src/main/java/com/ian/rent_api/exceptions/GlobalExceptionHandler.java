2c14fe5d89ee5f64c4928ae6dcb50bd08b0b4acc
Ian Sein <ianseingit10@gmail.com>
2025-04-15 19:54:24 -0300

    Add global exception handler class

---
+++ src/main/java/com/ian/rent_api/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,31 @@
+package com.ian.rent_api.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ProblemDetail handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+
+        for(FieldError fieldError : ex.getBindingResult().getFieldErrors()) {
+            errors.put(fieldError.getField(), fieldError.getDefaultMessage());
+        }
+
+        ProblemDetail problemDetail = ProblemDetail.forStatus(HttpStatus.BAD_REQUEST);
+        problemDetail.setTitle("Bad Request");
+        problemDetail.setDetail("Validation failed for one or more fields");
+        problemDetail.setProperty("errors", errors);
+
+        return problemDetail;
+    }
+}
